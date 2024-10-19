6fb6f5cd9d546ff2292da9abbf2e20dd99be25af
sanjukumari-tech <foodlazzez@gmail.com>
2024-10-19 13:57:27 +0530

    this is few questions asked in interview

---
+++ script.js
@@ -0,0 +1,14 @@
+// // const button1  = document.getElementById("button")
+// // button1.addEventListener("click",()=>{
+// //     alert("hello");
+// // })
+
+// // https://ffmpeg-dev-2.unscript.ai/ip
+
+//  async function   fetchData(){
+//      const res =  await fetch("https://ffmpeg-dev-2.unscript.ai/ip");
+//      const data = await res.json();
+//      console.log(data)
+//      return;
+// }
+// fetchData();
